c1a4487f378342b56e2ca39765c0ca04ea42ca1c
lafar6502 <rafal@cogit.pl>
2015-03-31 21:45:34 +0200

    save

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,25 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
+.controller('MapCtrl', function($scope, $ionicLoading, $http) {
+  var me = this;
+  this.categories = [
+	{"id":"c1", "name":"Restauracje"},
+	{"id":"c2", "name": "Sport i rekreacja"},
+	{"id":"c3", "name": "Rozrywka"}
+  ];
+
+  $http.get("http://www.marcysia.net/categories.json").success(function(data, status, headers, config) {
+		me.categories = data;
+		console.log(data);
+	});
   $scope.mapCreated = function(map) {
     $scope.map = map;
+  };
+  
+  this.categoryChanged = function() {
+	if (!$scope.map) {return;}
+	var bnds = $scope.map.getBounds();
+	console.log('bounds', bnds, 'cat:', me.categorySel);
   };
 
   $scope.centerOnMe = function () {
