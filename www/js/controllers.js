ca98483515597e4334f00b7c33bfa7e79a5ebf6e
lafar6502 <rafal@cogit.pl>
2015-04-02 18:34:41 +0200

    locations

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,83 +1,113 @@
 angular.module('starter.controllers', [])
 
 .controller('MapCtrl', function($scope, $ionicLoading, $http, $ionicModal) {
-  var me = this;
-  this.categories = [
-	{"id":"c1", "name":"Restauracje"},
-	{"id":"c2", "name": "Sport i rekreacja"},
-	{"id":"c3", "name": "Rozrywka"}
-  ];
-  $scope.categories = this.categories;
-  $scope.doLog = function(a) {
-	console.log(a);
-  };
+	var me = this;
+	this.categories = [];
+	this.markers = [];
 
-  $http.get("http://www.4youcard.pl/srv_api.php").success(function(data, status, headers, config) {
-		console.log(data);
-		$scope.categories = me.categories = data;
-		
+	$scope.categories = this.categories;
+	$scope.doLog = function (a) {
+		console.log(a);
+	};
+  
+	$http.get("http://www.4youcard.pl/srv_api.php").success(function(data, status, headers, config) {
+		$scope.categories = me.categories = data;	
 	});
+
 	$ionicModal.fromTemplateUrl('templates/category-select.html', {
 		scope: $scope
-	  }).then(function(modal) {
+	}).then(function(modal) {
 		$scope.modal = modal
-	  });
-	  
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
+	});
+
+	$scope.mapCreated = function(map) {
+		$scope.map = map;
+		$scope.centerOnMe();
+	};
+  
+	this.selectCategory = function() {
+		$scope.modal.show();
+	};
+  
+  var gInfow = null;
+  
+  var str = function(s) {
+	if (s == undefined || s == null) return '';
+	return s.toString();
   };
   
-  this.selectCategory = function() {
-	$scope.modal.show();
+  var configureMarker = function(mark, map) {
+	
+	var site = new google.maps.LatLng(mark.lat, mark.lng);
+	var marker = new google.maps.Marker({position: site, map: map,  title: mark.name});
+	me.markers.push(marker);
+	
+	google.maps.event.addListener(marker, 'click', function() {
+		var cnt = [
+			"<b>" + mark.name + "</b> <br/>",
+			"<small>" + str(mark.address1) + "</small><br/>",
+			"<small>" + mark.zipcode + "&nbsp;" + mark.city + "</small><br/>",
+			'<span style="color:red;font-size:large">' + mark.discount1 + "&nbsp;" + str(mark.discount2) + "&nbsp;" + str(mark.discount3) + '</span><br/>',
+			'<a href="' + mark.www + '">' + mark.www + '</a><br/>',
+			'<small>' + str(mark.category) + '</small>'
+		];
+		if (gInfow != null) {
+			gInfow.close();
+			gInfow = null;
+		};
+		var w = new google.maps.InfoWindow({
+			content: cnt.join("")
+		});
+		w.open(marker.get('map'), marker);
+		gInfow = w;
+		setTimeout(function() {w.close();}, 10000);
+	});
+	console.log('configured marker', mark);
   };
   
-  $scope.categoryClicked = function(id) {
-	$scope.modal.hide();
-	if (!$scope.map) {return;}
-	var bnds = $scope.map.getBounds();
-	console.log('bounds', bnds, 'cat:', me.categorySel);
-  };
+  
+  this.loadLocations = function() {
+	$http.get("http://www.marcysia.net/locations.php").success(function(data, status, headers, config) {
+		console.log('locations', data);
+		$scope.locations = data;
+		for (var i=0; i<me.markers.length; i++) {
+			me.markers[i].setMap(null);
+		};
+		me.markers = [];
+		
+		for (var i=0; i<data.length; i++) {
+			configureMarker(data[i], $scope.map);
+		};
+	});
+  }
+  
+	$scope.categoryClicked = function(id) {
+		$scope.modal.hide();
+		if (!$scope.map) {return;}
+		var bnds = $scope.map.getBounds();
+		console.log('bounds', bnds, 'cat:', me.categorySel);
+	};
 
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
-    }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.loading.hide();
-	  $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-    }, function (error) {
-      $scope.loading.hide();
-	  alert('Unable to get location: ' + error.message);
-    }, {timeout: 30000, enableHighAccuracy: false});
-  };
-  this.centerV2 = function() {
+	$scope.centerOnMe = function () {
 	console.log("Centering");
-    if (!$scope.map) {
-      return;
-    };
-		$scope.loading = $ionicLoading.show({
+	if (!$scope.map) {
+		return;
+	}
+
+	$scope.loading = $ionicLoading.show({
 		  content: 'Getting current location...',
 		  showBackdrop: false
-		});
-		var posOptions = {timeout: 30000, enableHighAccuracy: false};
-		$cordovaGeolocation.getCurrentPosition(posOptions)
-			.then(function (position) {
-			  var lat  = position.coords.latitude;
-			  var long = position.coords.longitude;
-			  $scope.map.setCenter(new google.maps.LatLng(lat, long));
-			  $scope.loading.hide();
-			}, function(err) {
-			  $scope.loading.hide();
-			  alert('Unable to get location: ' + err.message);
-			});
+	});
 
-  };
+	navigator.geolocation.getCurrentPosition(function (pos) {
+		  console.log('Got pos', pos);
+		  $scope.loading.hide();
+		  $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+	}, function (error) {
+		  $scope.loading.hide();
+		  alert('Unable to get location: ' + error.message);
+	}, {timeout: 30000, enableHighAccuracy: false});
+	};
+	//
+	this.loadLocations();
 });
