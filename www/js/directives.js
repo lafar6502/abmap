e1e2d57f18dcb30b89b85805a08ac798dc263d13
lafar6502 <rafal@cogit.pl>
2015-04-03 10:00:52 +0200

    mp nat

--- www/js/directives.js
+++ www/js/directives.js
@@ -8,7 +8,7 @@ angular.module('starter.directives', [])
     },
     link: function ($scope, $element, $attr) {
       function initialize() {
-        var mapOptions = {
+        /*var mapOptions = {
           center: new google.maps.LatLng(21.07493, 52.381388),
           zoom: 13,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -18,16 +18,28 @@ angular.module('starter.directives', [])
 		  scaleControl: true,
 		  streetViewControl: false,
 		  overviewMapControl: false
-        };
-        var map = new google.maps.Map($element[0], mapOptions);
+        };*/
+		
+		//alert('plu? ' + plugin + ', w.plu? ' + window.plugin);
+		try {
+			var map = plugin.google.maps.Map.getMap($element[0]);
+			map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
+				$scope.onCreate({map: map});
+			});
+		}
+		catch(e) {
+			alert('error: ' + e.toString());
+		};
+		
+        /*var map = new google.maps.Map($element[0], mapOptions);
   
-        $scope.onCreate({map: map});
+        
 
         // Stop the side bar from dragging when mousedown/tapdown on the map
         google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
           e.preventDefault();
           return false;
-        });
+        });*/
       }
 
       if (document.readyState === "complete") {
